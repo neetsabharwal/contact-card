95a6b3048319d4cb55115dce71fda5174d549dc3
Gurneet <gsingh2@atb.com>
2022-09-13 21:05:28 -0400

    delete done

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,5 +1,5 @@
 import "./form.js";
-import { clearForm,toggleForm } from "./form.js";
+import { clearForm, toggleForm } from "./form.js";
 
 // Import CSS files
 import "../css/index.css";
@@ -11,9 +11,9 @@ import Logo from "../images/logo.png";
 import Bear from "../images/bear.png";
 import Dog from "../images/dog.png";
 
-import { initdb, getDb, postDb } from "./database.js";
+import { initdb, getDb, postDb, deleteDb } from "./database.js";
 
-import {fetchCards} from "./cards.js";
+import { fetchCards } from "./cards.js";
 
 window.addEventListener("load", function () {
   initdb();
@@ -23,38 +23,46 @@ window.addEventListener("load", function () {
   document.getElementById("dogThumbnail").src = Dog;
 });
 
-  // Form functionality
-  const form = document.getElementById("formToggle");
-  const newContactButton = document.getElementById("new-contact");
-  let submitBtnToUpdate = false;
-  let profileId;
-  
-  newContactButton.addEventListener('click', event => {
-    toggleForm()
-   })
-  
-  form.addEventListener('submit', event => {
-    // Handle data
-    event.preventDefault();
+// Form functionality
+const form = document.getElementById("formToggle");
+const newContactButton = document.getElementById("new-contact");
+let submitBtnToUpdate = false;
+let profileId;
+
+newContactButton.addEventListener("click", (event) => {
+  toggleForm();
+});
+
+form.addEventListener("submit", (event) => {
+  // Handle data
+  event.preventDefault();
   let name = document.getElementById("name").value;
   let phone = document.getElementById("phone").value;
   let email = document.getElementById("email").value;
   let profile = document.querySelector('input[type="radio"]:checked').value;
-  
-    // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+
+  // Post form data to IndexedDB OR Edit an existing card in IndexedDB
   if (submitBtnToUpdate == false) {
     postDb(name, email, phone, profile);
   } else {
-  
     fetchCards();
-      // Toggles the submit button back to POST functionality
+    // Toggles the submit button back to POST functionality
     submitBtnToUpdate = false;
   }
-  
+
   // Clear form
   clearForm();
   // Toggle form
   toggleForm();
   // Reload the DOM
   fetchCards();
-  });
+});
+
+window.deleteCard = (e) => {
+  // Grabs the id from the button element attached to the contact card.
+  let id = parseInt(e.id);
+  // Delete the card
+  deleteDb(id);
+  // Reload the DOM
+  fetchCards();
+};
