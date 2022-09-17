9754189525d0cc63bd4fe5c10c29ad8d3348b384
Gurneet <gsingh2@atb.com>
2022-09-16 21:05:51 -0400

    add to staging lesson 4

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -26,6 +26,22 @@ window.addEventListener("load", function () {
 // Form functionality
 const form = document.getElementById("formToggle");
 const newContactButton = document.getElementById("new-contact");
+
+const installBtn = document.getElementById(`installBtn`);
+window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault();
+  installBtn.style.visibility = 'visible';
+installBtn.addEventListener('click', () => {
+  event.prompt();
+  installBtn.setAttribute('disabled', true);
+  installBtn.textContent = 'Installed!';
+});
+});
+
+window.addEventListener('appinstalled', (event) => {
+  console.log('👍', 'appinstalled', event);
+});
+
 let submitBtnToUpdate = false;
 let profileId;
 
